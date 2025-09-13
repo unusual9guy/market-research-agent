dd2898480ef84c9561f4e5dcb610167ba202741e
unusual9guy <vanshgoenka007@gmail.com>
2025-09-13 12:56:29 +0530

    updated init to initialise the report generator

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,9 @@ Utility functions and helpers for the Market Research Agent system.
 """
 
 from .web_searcher import WebSearcher
+from .report_generator import ReportGenerator
 
 __all__ = [
-    "WebSearcher"
+    "WebSearcher",
+    "ReportGenerator"
 ]
