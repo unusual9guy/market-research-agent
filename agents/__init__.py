1c24a48aa00faf84db0ea7330659f78a3dd1fa08
unusual9guy <vanshgoenka007@gmail.com>
2025-09-13 13:23:08 +0530

    updated init file for new agents

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,8 +5,10 @@ This package contains the individual agents that work together to perform
 automated market research and AI use case generation.
 """
 
-from .industry_research_agent import IndustryResearchAgent
+from .enhanced_industry_agent import EnhancedIndustryResearchAgent
+from .enhanced_use_case_agent import EnhancedUseCaseGenerationAgent
 
 __all__ = [
-    "IndustryResearchAgent"
+    "EnhancedIndustryResearchAgent",
+    "EnhancedUseCaseGenerationAgent"
 ]
