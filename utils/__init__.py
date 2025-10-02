821f2f8025f86c6b3f8fdba7144dd7391163fc0b
unusual9guy <vanshgoenka007@gmail.com>
2025-10-02 16:58:09 +0530

    init file

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,8 +4,10 @@ Utility functions and helpers for the Market Research Agent system.
 
 from .web_searcher import WebSearcher
 from .report_generator import ReportGenerator
+from .md_to_pdf import convert_md_to_pdf
 
 __all__ = [
     "WebSearcher",
-    "ReportGenerator"
+    "ReportGenerator",
+    "convert_md_to_pdf"
 ]
